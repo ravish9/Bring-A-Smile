c0b799e493e6161922bfb93406db24a271338b13
Shaivin Bhekharee <bshaivin@gmail.com>
2020-03-05 07:31:31 +0400

    Organisation

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,34 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.content.Intent;
+import com.google.firebase.auth.FirebaseAuth;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button button4;
+    FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        button4 = findViewById(R.id.button);
+
+
+
+
+ button4.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+
+        Intent intToShopping = new Intent(MainActivity.this, Organisation.class);
+        startActivity(intToShopping);
+        }
+        });
+        }}
